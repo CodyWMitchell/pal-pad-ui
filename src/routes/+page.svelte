663d6d810474cbd110a7c7b1dfdfb6713981324d
Cody Mitchell <comitche@redhat.com>
2023-02-27 11:06:47 -0500

    Initialize Svelte project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<div class="w-full text-center mt-4">
+	<h1 class="text-xl font-bold">Welcome to Pal Pad</h1>
+	<p>
+		Visit <a href="https://github.com/CodyWMitchell/pal-pad-ui" class="text-blue-500 underline">Our GitHub</a> to read the documentation
+	</p>
+</div>
