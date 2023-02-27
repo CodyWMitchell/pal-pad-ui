0a4879072f548c51d5a886afa6e95415841c44cb
Cody Mitchell <comitche@redhat.com>
2023-02-27 12:10:35 -0500

    Add random color button and size slider for testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,17 @@
 
 	let width = 700;
 	let height = 700;
+	let brushSize = 30;
 
 	let sketch = (p5) => {
 		p5.setup = () => {
 			p5.createCanvas(width, height);
 			p5.background(200);
-            p5.frameRate(60);
+			p5.frameRate(60);
 		};
 
 		p5.mouseDragged = () => {
-			p5.strokeWeight(4);
+			p5.strokeWeight(brushSize);
 			p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
 		};
 
@@ -21,18 +22,46 @@
 				p5.background(200);
 			}
 		};
+
+		let randomColor = document.getElementById('randomColor');
+		randomColor.addEventListener('click', () => {
+			let color = [p5.random(255), p5.random(255), p5.random(255)];
+			p5.stroke(color);
+
+			randomColor.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+
+			if (color[0] + color[1] + color[2] < (255 * 3) / 2) {
+				randomColor.style.color = 'white';
+			} else {
+				randomColor.style.color = 'black';
+			}
+		});
+
+		let brushSlider = document.getElementById('brushSlider');
+		brushSlider.addEventListener('input', () => {
+			brushSize = brushSlider.value;
+		});
 	};
 </script>
 
 <div class="w-full text-center justify-center mt-4">
 	<h1 class="text-xl font-bold">Welcome to Pal Pad</h1>
 	<p>click and drag to draw, press [SPACE] to clear</p>
-	<div class="w-full flex justify-center">
+	<div class="w-full flex justify-center p-2">
 		<P5 {sketch} />
 	</div>
+	<button
+		id="randomColor"
+		class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+	>
+		Random Color
+	</button>
+	Size: <input id="brushSlider" type="range" min="1" max="100" value="4" class="slider" />
 	<p>
-		Visit <a href="https://github.com/CodyWMitchell/pal-pad-ui" class="text-blue-500 underline"
+		Visit
+		<a href="https://github.com/CodyWMitchell/pal-pad-ui" class="text-blue-500 underline"
 			>Our GitHub</a
-		> to read the documentation
+		>
+		to read the documentation
 	</p>
 </div>
